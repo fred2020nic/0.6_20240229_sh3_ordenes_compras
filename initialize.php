158be7be5a83f663257742a3044dc6fe1c63a1d5
fred2020nic <fep.200978@gmail.com>
2024-02-29 12:38:44 -0600

    login

--- initialize.php
+++ initialize.php
@@ -19,7 +19,7 @@ if(!defined('DB_NAME')) define('DB_NAME',"0.6_20240229_sh3_ordenes_compras");
 
 }else{
     $dev_data = array('id'=>'-1','firstname'=>'Mauricio','Sevilla'=>'','username'=>'configuroweb','password'=>'4b67deeb9aba04a5b54632ad19934f26','last_login'=>'','date_updated'=>'','date_added'=>'');
-    if(!defined('base_url')) define('base_url','http://fepapp.com/sh3_ordenes_compras/');
+    if(!defined('base_url')) define('base_url','https://fepapp.com/sh3_ordenes_compras/');
     if(!defined('base_app')) define('base_app', str_replace('\\','/',__DIR__).'/' );
     if(!defined('dev_data')) define('dev_data',$dev_data);
     if(!defined('DB_SERVER')) define('DB_SERVER',"localhost");
